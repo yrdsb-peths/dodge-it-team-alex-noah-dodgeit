b5b7bc586d04fa8198a639881e2753bd4e6c881e
alexzhao3487-boop <alexzhao3487@gmail.com>
2026-04-20 11:34:08 -0400

    added hero and baby spawning

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,11 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Hero hero = new Hero();
+        addObject(hero, 100, 100); 
+        
+        Baby baby = new Baby();
+        addObject(baby, 600, 100);
     }
 }
