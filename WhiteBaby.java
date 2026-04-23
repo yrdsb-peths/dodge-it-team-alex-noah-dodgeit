9da43c7ca0f740fcd0a0f4272b5e184ecd84d851
lumbers <noahlum2009@gmail.com>
2026-04-23 10:55:50 -0400

    Update WhiteBaby.java

--- WhiteBaby.java
+++ WhiteBaby.java
@@ -12,6 +12,11 @@ public class WhiteBaby extends Actor
      * Act - do whatever the WhiteBaby wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public WhiteBaby()
+    {
+        GreenfootImage image = new GreenfootImage("man.png");
+        setImage(image);
+    }
     public void act()
     {
         // Add your action code here.
